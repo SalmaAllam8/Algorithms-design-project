3a31a99f93529b0c51e195fbbdc4e00c1baa844d
maramismail9494 <ramoismail94@gmail.com>
2023-05-17 00:37:26 +0300

    Update algorithm project.py

--- algorithm project.py
+++ algorithm project.py
@@ -77,10 +77,33 @@ def printJobScheduling(arr, t):
 
 # Example2
 
+#please note in this question the answer will varie from 10 to 11 if we switched the sorting from the ending time to the starting time
+#but we chose the ending time because it is only better in most cases 
+def find_m(jobs):
+    jobs = sorted(jobs, key=lambda x : x[1])
+    #intializing the profit start time and end time of a job with the first job in the sorted list
+    prev_s =jobs[0][0]
+    prev_e = jobs[0][1]
+    profit = jobs[0][2]
+
+  
+    for s, e, p in jobs:
+        #making sure the starting time of the new job is greater than or equal to the previous job 
+        if s >= prev_e:
+            #pass
+    #else:
+    #adding the new values of the new job to our variables 
+           prev_s, prev_e = s,e
+           profit += p
+    return profit
+jobs = [(1, 6, 6), (2, 5, 5), (5, 7, 5), (6, 8, 3)]
+max_profit = find_m(jobs)
+print(max_profit)
 
 
+#another answer
 def find_max_profit(jobs) :
-    #the jobs are sorted in a descending order according to the profit to priorities the high profit jobs  
+    #the jobs are sorted in a ascending order according to the profit to priorities the jobs which will end faster to maximize the profit 
     jobs = sorted(jobs, key=lambda x : x[1])
 #intializing our schedule and max_profit 
     schedule = []
